87c8e8c723785ce338b11c88ac670a7d2fce55a6
Tatsuya-Nonogaki <winfield@support.email.ne.jp>
2025-04-10 15:17:40 +0900

    New Repository

---
+++ JobList.ps1
@@ -0,0 +1,201 @@
+<#
+ .SYNOPSIS
+  Output Job List.
+
+ .DESCRIPTION
+  Output Job List to a CSV file.
+  Version: 0.2.4
+
+  The output CSV consists of following fields.
+  
+  *Name
+    Job Name.
+  
+  *JobType
+    Backup, Replica, etc.
+  
+  *Description
+    Description field of the Job.
+  
+  *Sources
+    Source objects of backup/replication.
+  
+  *TargetRepository, TargetDatastore, etc.
+    Various properties depending on the job 'Type' argument.
+  
+  *RestorePoints
+    Restore points to keep.
+  
+  *IsScheduleEnabled
+    'FALSE' indicates the Job will not run automatically because of 'Disable' operation 
+    on the Jobs list pane.
+  
+  *RunAutomatically
+    'FALSE' indicates the Job will not run automatically because 'Run the job automatically' 
+    check box is unchecked in Schedule page of the Job configuration.
+  
+  *IsRunning
+    The Job was running at the moment the list was acquired.
+  
+  *LastRun
+    The most recent start time of the Job.
+  
+  *LastResult
+    Result of the LastRun.
+  
+  *DailyStartTime
+    Configured start time at the 'Daily at this time' field of Schedule page of the Job 
+    configuration. Usually ignorable on a Periodically scheduled Job.
+  
+  *Periodically
+    Interval configured at 'Periodically every' field of the Schedule.
+  
+  *HourlyOffset
+    Time offset configured at 'Start time within an hour' field of Periodical Schedule.
+ 
+ .PARAMETER Type
+  (Alias -t) Mandatory. Job type. Must be either 'backup' or 'replica'.
+ 
+ .PARAMETER Log
+  (Alias -l) Output file path. If it doesn't contain '\', it is assumed in the script dir.
+  Defaults to 'scriptdir\joblist.csv'.
+ 
+ .PARAMETER Stat
+  (Alias -s) Let the output include status related columns, i.e., IsRunning, LastRun 
+  and LastResult. 
+#>
+[CmdletBinding()]
+Param(
+  [Parameter(Mandatory=$true)]
+  [ValidateSet("backup", "replica")]
+  [Alias("t")]
+  [string]$Type,
+
+  [Parameter()]
+  [Alias("l")]
+  [string]$Log,
+
+  [Parameter()]
+  [Alias("s")]
+  [switch]$Stat
+)
+
+# File to append logs to. Comment it out or keep null if no logging is required.
+$Set_Log = 'joblist.csv'
+
+$scriptdir = Split-Path -Path $myInvocation.MyCommand.Path -Parent
+
+import-module Veeam.Backup.PowerShell -warningaction silentlycontinue
+
+if ($Log) {
+    $logname = $Log
+} elseif ($Set_Log) {
+    $logname = $Set_Log
+}
+
+if ($logname) {
+    if ($logname.contains('\')) {
+        $logfile = $logname
+    } else {
+        $logfile = $scriptdir + '\' + $logname
+    }
+
+    $logdir = Split-Path -Path $logfile -Parent
+
+    if (!(Test-Path $logdir)) {
+        Write-Error "Log directory $logdir does not exist"
+        exit 1
+    }
+}
+
+function Get-JobData {
+    param(
+        [Parameter()]
+        [Object]$job,
+        [Parameter()]
+        [string]$Type
+    )
+
+    $commonProps = @{
+        Name = $job.Name
+        JobType = $job.JobType
+        Description = $job.Description
+        Sources = $job.GetObjectsInJob().Name -join ";"
+        RestorePoints = if ($job.BackupStorageOptions.RetentionType -eq "Cycles") { $job.BackupStorageOptions.RetainCycles, "points" -join " " } else { $job.BackupStorageOptions.RetainCycles, "days" -join " " }
+        IsScheduleEnabled = $job.IsScheduleEnabled
+        RunAutomatically = -not $job.Options.JobOptions.RunManually
+        IsRunning = $job.IsRunning
+        LastRun = if ((Get-Date $job.LatestRunLocal) -gt (Get-Date 1970-01-01)) { $job.LatestRunLocal } else { $null }
+        LastResult = if ((Get-Date $job.LatestRunLocal) -gt (Get-Date 1970-01-01)) { $job.GetLastResult() } else { $null }
+        DailyStartTime = if ($job.ScheduleOptions.OptionsDaily.TimeLocal) { $job.ScheduleOptions.OptionsDaily.TimeLocal | Get-Date -Format t } else { $null }
+        Periodically = if ($job.ScheduleOptions.OptionsPeriodically.Enabled -eq "True") { ($job.ScheduleOptions.OptionsPeriodically -split ",")[1] -replace '^[^,]*:\s*', '' } else { $null }
+        HourlyOffset = if ($job.ScheduleOptions.OptionsPeriodically.Enabled -eq "True") { $job.ScheduleOptions.OptionsPeriodically.HourlyOffset } else { $null }
+    }
+
+    $specificProps = @{
+        TargetRepository = $job.GetTargetRepository().Name
+        TargetCluster = $job.ViReplicaTargetOptions.ClusterName
+        TargetFolder = $job.ViReplicaTargetOptions.ReplicaTargetVmFolderName
+        TargetDatastore = $job.ViReplicaTargetOptions.DatastoreName
+    }
+
+    if ($Type -eq "backup") {
+        $orderedProps = [PSCustomObject]@{
+            Name = $commonProps.Name
+            JobType = $commonProps.JobType
+            Description = $commonProps.Description
+            Sources = $commonProps.Sources
+            TargetRepository = $specificProps.TargetRepository
+            RestorePoints = $commonProps.RestorePoints
+            IsScheduleEnabled = $commonProps.IsScheduleEnabled
+            RunAutomatically = $commonProps.RunAutomatically
+            DailyStartTime = $commonProps.DailyStartTime
+            Periodically = $commonProps.Periodically
+            HourlyOffset = $commonProps.HourlyOffset
+        }
+    } elseif ($Type -eq "replica") {
+        $orderedProps = [PSCustomObject]@{
+            Name = $commonProps.Name
+            JobType = $commonProps.JobType
+            Description = $commonProps.Description
+            Sources = $commonProps.Sources
+            TargetCluster = $specificProps.TargetCluster
+            TargetFolder = $specificProps.TargetFolder
+            TargetDatastore = $specificProps.TargetDatastore
+            RestorePoints = $commonProps.RestorePoints
+            IsScheduleEnabled = $commonProps.IsScheduleEnabled
+            RunAutomatically = $commonProps.RunAutomatically
+            DailyStartTime = $commonProps.DailyStartTime
+            Periodically = $commonProps.Periodically
+            HourlyOffset = $commonProps.HourlyOffset
+        }
+    }
+
+    if ($Stat) {
+        $orderedProps | Add-Member -MemberType NoteProperty -Name "IsRunning" -Value $commonProps.IsRunning
+        $orderedProps | Add-Member -MemberType NoteProperty -Name "LastRun" -Value $commonProps.LastRun
+        $orderedProps | Add-Member -MemberType NoteProperty -Name "LastResult" -Value $commonProps.LastResult
+    }
+
+    return $orderedProps
+}
+
+$jobs = Get-VBRJob
+if ($jobs) {
+    $filteredJobs = switch ($Type) {
+        "backup" { $jobs | Where-Object { $_.JobType -eq "Backup" } }
+        "replica" { $jobs | Where-Object { $_.JobType -eq "Replica" } }
+    }
+
+    if ($filteredJobs.Count -eq 0) {
+        Write-Host "No matching jobs found"
+        Exit 1
+    }
+
+    $jobData = $filteredJobs | ForEach-Object { Get-JobData -job $_ -Type $Type }
+
+    $jobData | Export-Csv -Path $logfile -Encoding UTF8 -NoTypeInformation
+} else {
+    Write-Host "No Job output"
+    Exit 1
+}
---
+++ JobStatus.ps1
@@ -0,0 +1,129 @@
+<#
+ .SYNOPSIS
+  Get the last result of the VBR Job.
+
+ .DESCRIPTION
+  Get the last result of specified VBR Job. The job status can be grasped 
+  through log file and/or exit code of the script.
+  Version: 0.6.2
+  
+  Exit codes:
+       0: Job finished with Success status.
+       1: Job finished with Failed status.
+       2: Job finished with Warning.
+       4: The job has never been run yet ("None" status).
+       8: Unknown.
+       *: Other errors. ie: non-existent JobName, syntax errors, etc.
+
+ .PARAMETER JobName
+  (Alias -j) Mandatory. The definition name of Job.
+
+ .PARAMETER Log
+  (Alias -l) Append logs to this file. This overrides $Set_Log parameter in
+  the script itself. If it doesn't contain '\', it is assumed in the script 
+  dir. No logging is done if neither Log nor Set_Log are set.
+
+ .PARAMETER NoLog
+  Disable logging even if $Sset_Log parameter is set or Log option is passed.
+
+ .PARAMETER Quiet
+  (Alias -q) Suppress console output. Result can be grasped through exit 
+  code or log file.
+#>
+[CmdletBinding()]
+Param(
+  [Parameter(Position=0)]
+  [Alias("j")]
+  [string]$JobName,
+
+  [Parameter()]
+  [Alias("l")]
+  [string]$Log,
+
+  [Parameter()]
+  [switch]$NoLog,
+
+  [Parameter()]
+  [Alias("q")]
+  [switch]$Quiet
+)
+
+# File to append logs to. Comment it out or keep null if no logging is required at all.
+$Set_Log = 'jobstatus.log'
+
+$scriptdir = Split-Path -Path $myInvocation.MyCommand.Path -Parent
+
+import-module Veeam.Backup.PowerShell -warningaction silentlycontinue
+
+if ($JobName.length -eq 0) {
+    Get-Help $myInvocation.MyCommand.Path
+    Exit 254
+}
+
+if ($Log) {
+    $logname = $Log
+} elseif ($Set_Log) {
+    $logname = $Set_Log
+}
+
+if ($logname -and !$NoLog) {
+    if ($logname.contains('\')) {
+        $logfile = $logname
+    } else {
+        $logfile = $scriptdir + '\' + $logname
+    }
+
+    $logdir = Split-Path -Path $logfile -Parent
+
+    if (Test-Path $logdir) {
+        $logging = $True
+    } else {
+        Write-Warning "Log directory $logdir does not exist, logging is disabled"
+    }
+}
+
+Function WriteMsg {
+    param (
+        [Parameter(Position=0)]
+        [string]$Msg
+    )
+
+    if (!$Quiet) {
+        Write-Host "$Msg"
+    }
+
+    if ($logging) {
+        '{0}	[{1}] {2}' -f $(Get-Date -Format "yyyy/MM/dd HH:mm:ss"), $JobName, $Msg | Out-File -FilePath $logfile -Encoding Default -Append
+    }
+}
+
+$job = Get-VBRJob -Name $JobName
+if (!$job) {
+    WriteMsg "No such Job by that name or unable to talk to VBR Server"
+    Exit 254
+}
+
+$retval = 8
+
+$LastRun = if ((Get-Date $job.LatestRunLocal) -gt (Get-Date 1970-01-01)) { Get-Date $job.LatestRunLocal -Format "yyyy/MM/dd-HH:mm:ss" } else { $null }
+$LastResult = if ((Get-Date $job.LatestRunLocal) -gt (Get-Date 1970-01-01)) { $job.GetLastResult() } else { $null }
+
+$LastSession = $job.FindLastSession()
+if ($LastSession) {
+    $SessionStart = if ((Get-Date $LastSession.CreationTime) -gt (Get-Date 1970-01-01)) { Get-Date $LastSession.CreationTime -Format "yyyy/MM/dd-HH:mm:ss" } else { $null }
+    $SessionEnd = if ((Get-Date $LastSession.EndTime) -gt (Get-Date 1970-01-01)) { Get-Date $LastSession.EndTime -Format "yyyy/MM/dd-HH:mm:ss" } else { $null }
+    $SessionMode = if ($LastSession.IsFullMode) { "Full" } else { "NotFull" }
+} else {
+    WriteMsg "No such Session record"
+}
+
+Switch ($LastResult) {
+  'Success' { $retval = 0 }
+  'Failed'  { $retval = 1 }
+  'Warning' { $retval = 2 }
+  'None'    { $retval = 4 }
+}
+
+WriteMsg "Result:$LastResult Code:$retval LastRun:$LastRun SessionStart:$SessionStart SessionEnd:$SessionEnd Mode:$SessionMode"
+
+Exit $retval
