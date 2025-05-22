0b58395aa304ef9b4a8bd6c4d5ca22f9bec42a5f
tatjanon <winfield@support.email.ne.jp>
2025-05-22 14:14:02 +0900

    Update JobList.ps1 0.4beta

--- JobList.ps1
+++ JobList.ps1
@@ -4,25 +4,27 @@
 
  .DESCRIPTION
   Output Job List to a CSV file.
-  Version: 0.3.4
+  Version: 0.4.0beta
 
-  The output CSV consists of following fields.
-  
+  The output CSV consists of the following fields,
+  which may vary depending on the job 'Type' argument.
+
   *Name
     Job Name.
-  
+
   *JobType
-    Backup, Replica, etc.
-  
+    Backup, Replica, SureBackup, etc.
+
   *Description
     Description field of the Job.
-  
-  *Sources
-    Source objects of backup/replication.
-  
+
+  *Sources / LinkedJob
+    - For Backup/Replica: Source objects of backup/replication.
+    - For SureBackup: Linked Job (placeholder).
+
   *TargetRepository, TargetDatastore, etc.
     Various properties depending on the job 'Type' argument.
-  
+
   *RestorePoints
     Restore points to keep.
   
@@ -46,23 +48,23 @@
   
   *IsRunning
     The Job was running at the moment the list was acquired.
-  
+
   *LastResult
     Result of the last job session.
-  
+
   *SessionStart and SessionEnd
     Start and end time of the last job session.
-  
+
   *Duration
     Duration of the last job calculated from SessionStart and SessionEnd.
- 
+
  .PARAMETER Type
-  (Alias -t) Mandatory. Job type. Must be either 'backup' or 'replica'.
- 
+  (Alias -t) Mandatory. Job type. Must be one of 'backup', 'replica', 'surebackup'.
+
  .PARAMETER Log
   (Alias -l) Output file path. If it doesn't contain '\', it is assumed in the script dir.
   Defaults to 'scriptdir\joblist-{Type}.csv'.
- 
+
  .PARAMETER Stat
   (Alias -s) Let the output include status related columns, i.e., IsRunning, LastResult, 
   SessionStart, SessionEnd and Duration. 
@@ -70,7 +72,7 @@
 [CmdletBinding()]
 Param(
   [Parameter(Mandatory=$true)]
-  [ValidateSet("backup", "replica")]
+  [ValidateSet("backup", "replica", "surebackup")]
   [Alias("t")]
   [string]$Type,
 
@@ -129,8 +131,8 @@ function Get-JobData {
         if ($SessionStart -and $SessionEnd) {
             $startTime = [datetime]::ParseExact($SessionStart, "yyyy/MM/dd HH:mm:ss", $null)
             $endTime = [datetime]::ParseExact($SessionEnd, "yyyy/MM/dd HH:mm:ss", $null)
-            $duration = $endTime - $startTime
-            $formattedDuration = $duration.ToString("hh\:mm\:ss")
+            $formattedDuration = $endTime - $startTime
+            $formattedDuration = $formattedDuration.ToString("hh\:mm\:ss")
         } else {
             $formattedDuration = $null
         }
@@ -200,22 +202,80 @@ function Get-JobData {
     return $orderedProps
 }
 
-$jobs = Get-VBRJob
-if ($jobs) {
-    $filteredJobs = switch ($Type) {
-        "backup" { $jobs | Where-Object { $_.JobType -eq "Backup" } }
-        "replica" { $jobs | Where-Object { $_.JobType -eq "Replica" } }
+function Get-SureBackupJobData {
+    param(
+        [Parameter()]
+        [Object]$job
+    )
+
+    $LastSession = $job.FindLastSession()
+
+    if ($LastSession) {
+        $SessionStart  = if ((Get-Date $LastSession.CreationTime) -gt (Get-Date 1970-01-01)) { Get-Date $LastSession.CreationTime -Format "yyyy/MM/dd HH:mm:ss" } else { $null }
+        $SessionResult = if ($SessionStart) { $LastSession.Result } else { $null }
+        $SessionEnd    = if ((Get-Date $LastSession.EndTime) -gt (Get-Date 1970-01-01)) { Get-Date $LastSession.EndTime -Format "yyyy/MM/dd HH:mm:ss" } else { $null }
+
+        if ($SessionStart -and $SessionEnd) {
+            $startTime = [datetime]::ParseExact($SessionStart, "yyyy/MM/dd HH:mm:ss", $null)
+            $endTime = [datetime]::ParseExact($SessionEnd, "yyyy/MM/dd HH:mm:ss", $null)
+            $formattedDuration = $endTime - $startTime
+            $formattedDuration = $formattedDuration.ToString("hh\:mm\:ss")
+        } else {
+            $formattedDuration = $null
+        }
     }
 
-    if ($filteredJobs.Count -eq 0) {
-        Write-Host "No matching jobs found"
+    # Placeholders for SureBackup-specific fields.
+    # You can update these as you explore the SureBackup job object properties.
+    $props = [PSCustomObject]@{
+        Name = $job.Name
+        JobType = "SureBackup"
+        Description = $job.Description
+        LinkedJob = $job.LinkedJob.Job.Name -join ";"
+        IsScheduleEnabled = $job.IsEnabled
+        RunAutomatically = $job.ScheduleEnabled
+        DailyStartTime = $null # Placeholder: fill with actual property if found
+        Periodically = $null   # Placeholder: fill with actual property if found
+        HourlyOffset = $null   # Placeholder: fill with actual property if found
+    }
+
+    if ($Stat) {
+        $props | Add-Member -MemberType NoteProperty -Name "IsRunning" -Value $job.IsRunning
+        $props | Add-Member -MemberType NoteProperty -Name "LastResult" -Value $SessionResult
+        $props | Add-Member -MemberType NoteProperty -Name "SessionStart" -Value $SessionStart
+        $props | Add-Member -MemberType NoteProperty -Name "SessionEnd" -Value $SessionEnd
+        $props | Add-Member -MemberType NoteProperty -Name "Duration" -Value $formattedDuration
+    }
+
+    return $props
+}
+
+if ($Type -eq "surebackup") {
+    $jobs = Get-VBRSureBackupJob
+    if ($jobs) {
+        $jobData = $jobs | ForEach-Object { Get-SureBackupJobData -job $_ }
+        $jobData | Export-Csv -Path $logfile -Encoding UTF8 -NoTypeInformation
+    } else {
+        Write-Host "No SureBackup jobs found"
         Exit 1
     }
-
-    $jobData = $filteredJobs | ForEach-Object { Get-JobData -job $_ -Type $Type }
-
-    $jobData | Export-Csv -Path $logfile -Encoding UTF8 -NoTypeInformation
 } else {
-    Write-Host "No Job output"
-    Exit 1
+    $jobs = Get-VBRJob
+    if ($jobs) {
+        $filteredJobs = switch ($Type) {
+            "backup" { $jobs | Where-Object { $_.JobType -eq "Backup" } }
+            "replica" { $jobs | Where-Object { $_.JobType -eq "Replica" } }
+        }
+
+        if ($filteredJobs.Count -eq 0) {
+            Write-Host "No matching jobs found"
+            Exit 1
+        }
+
+        $jobData = $filteredJobs | ForEach-Object { Get-JobData -job $_ -Type $Type }
+        $jobData | Export-Csv -Path $logfile -Encoding UTF8 -NoTypeInformation
+    } else {
+        Write-Host "No Job output"
+        Exit 1
+    }
 }
